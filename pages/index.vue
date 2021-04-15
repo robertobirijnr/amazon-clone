0e53447f6ea4426dc5fbcd77483efaabc911c7c9
robertobirijnr <robertobirijnr@hotmail.com>
2021-04-15 14:06:09 +0000

    admin app

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <div class="a-spacing-large">
+
+    </div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div class="com-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">All Products</h1>
+              <div class="a-spacing-large"></div>
+
+
+          <a href="#" class="a-button-buy-again">Add a new product</a>
+           <a href="#" class="a-button-history margin-right-10">Add a new category</a>
+             <a href="#" class="a-button-buy-again">Add a new owner</a>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style  scoped>
+
+</style>
